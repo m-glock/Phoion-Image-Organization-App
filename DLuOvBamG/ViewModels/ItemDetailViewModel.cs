4ab3ee13336df2c8031e1fed8288464dd5721090
Leonid <leonidb93@gmail.com>
2020-05-08 18:38:04 +0200

    initial, set up project, add gitignore

---
+++ DLuOvBamG/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DLuOvBamG.Models;
+
+namespace DLuOvBamG.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
