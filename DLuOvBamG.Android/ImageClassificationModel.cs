b452b8e1322179f6823c5ce9700968226a79563a
Leonid <leonidb93@gmail.com>
2020-05-19 02:41:43 +0200

    import catDogSloth model, add example classes from mushroom detector tutorial

---
+++ DLuOvBamG.Android/ImageClassificationModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DLuOvBamG.Droid
+{
+    public class ImageClassificationModel
+    {
+        public ImageClassificationModel(string tagName, float probability)
+        {
+            TagName = tagName;
+            Probability = probability;
+        }
+
+        public float Probability { get; }
+        public string TagName { get; }
+    }
+
+}
