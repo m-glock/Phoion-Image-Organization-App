20e32d50c241e4b42554ee425fc6d5e736bb3999
Mareike <m-glock@users.noreply.github.com>
2020-05-20 16:19:32 +0200

    changed Image class to Picture class

--- DLuOvBamG/ViewModels/ScanResultViewModel.cs
+++ DLuOvBamG/ViewModels/ScanResultViewModel.cs
@@ -15,46 +15,41 @@ namespace DLuOvBamG.ViewModels
     public class ScanResultViewModel : BaseViewModel
     {
 
-		public List<Models.Image> GalleryImages { get; private set; }
-		public List<Models.Image> GalleryImages2 { get; private set; }
-		public IList<Models.Image> SelectedImages { get; set; }
+		public List<Picture> GalleryImages { get; private set; }
+		public List<Picture> GalleryImages2 { get; private set; }
+		public IList<Picture> SelectedImages { get; set; }
 
 		public ScanResultViewModel()
         {
-			SelectedImages = new List<Models.Image>();
-			GalleryImages = new List<Models.Image>
-			{
-				new Models.Image("bla", "katzen.jpg"),
-				new Models.Image("bla", "FB4_IMI.jpg"),
-				new Models.Image("bla", "squirrel.jpg"),
-				new Models.Image("bla", "sdp.jpg"),
-				new Models.Image("bla", "volleyball.png"),
-				new Models.Image("bla", "katzen2.jpg"),
+			GalleryImages = new List<Picture>();
+			GalleryImages2 = new List<Picture>();
+			SelectedImages = new List<Picture>();
+
+			string[] images = {
+				"https://farm9.staticflickr.com/8625/15806486058_7005d77438.jpg",
+				"https://farm5.staticflickr.com/4011/4308181244_5ac3f8239b.jpg",
+				"https://farm8.staticflickr.com/7423/8729135907_79599de8d8.jpg",
+				"https://farm3.staticflickr.com/2475/4058009019_ecf305f546.jpg",
+				"https://farm6.staticflickr.com/5117/14045101350_113edbe20b.jpg",
+				"https://farm8.staticflickr.com/7524/15620725287_3357e9db03.jpg",
+				"https://farm9.staticflickr.com/8351/8299022203_de0cb894b0.jpg",
 			};
 
-			GalleryImages2 = new List<Models.Image>
+			for (int i = 0; i < images.Length; i++)
 			{
-				new Models.Image("bla", "volleyball.png"),
-				new Models.Image("bla", "sdp.jpg"),
-				new Models.Image("bla", "katzen2.jpg"),
-				new Models.Image("bla", "katzen.jpg"),
-				new Models.Image("bla", "squirrel.jpg"),
-				new Models.Image("bla", "FB4_IMI.jpg"),
-				
-			};
+				Picture picture = new Picture
+				{
+					Id = i.ToString(),
+					Uri = images[i]
+				};
+				GalleryImages.Add(picture);
+				GalleryImages2.Add(picture);
+			}
 
 			Console.WriteLine("Created gallery images.");
 		}
 
-		public void printSelectedItems()
-		{
-			foreach (Models.Image image in SelectedImages)
-			{
-				Console.WriteLine(image.Name + " is currently selected.");
-			}
-		}
-
-		public void changeSelection(IList<Models.Image> selected)
+		public void changeSelection(IList<Picture> selected)
 		{
 			SelectedImages = selected;
 		}
