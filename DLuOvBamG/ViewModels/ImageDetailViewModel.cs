d12b7935732fba0a3aff4c55c1169cf9162ed137
m-glock <mareike.glock@t-online.de>
2020-05-30 11:08:45 +0200

    removed main page and retrieved ImageDetailViewModel

---
+++ DLuOvBamG/ViewModels/ImageDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using DLuOvBamG.Models;
+
+namespace DLuOvBamG.ViewModels
+{
+    public class ImageDetailViewModel : BaseViewModel
+    {
+        public Picture Image { get; set; }
+        public ImageDetailViewModel(Picture item)
+        {
+            Image = item;
+        }
+    }
+}
