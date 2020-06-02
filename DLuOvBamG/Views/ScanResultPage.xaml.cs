efcbbacb40f1ee75378eee72f24fcc5d1b389e5a
m-glock <mareike.glock@t-online.de>
2020-06-02 13:21:44 +0200

    ides for showing scanresults in folders/groups

--- DLuOvBamG/Views/ScanResultPage.xaml.cs
+++ DLuOvBamG/Views/ScanResultPage.xaml.cs
@@ -10,52 +10,14 @@ namespace DLuOvBamG.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ScanResultPage : ContentPage
 	{
-		ScanResultViewModel vm;
+		ScanOptionResultsViewModel vm;
+		private Dictionary<ScanOptionsEnum, double> options;
 
 		public ScanResultPage(Dictionary<ScanOptionsEnum, double> options)
 		{
+			Title = "Option 1";
 			InitializeComponent();
-			vm = BindingContext as ScanResultViewModel;
-            foreach (KeyValuePair<ScanOptionsEnum, double> pair in options)
-			{
-				addCollectionViewToPage(pair.Key);
-				Console.WriteLine("Option: " + pair.Key.ToString() + " with value " + pair.Value);
-			}
-		}
-
-		void ImageSelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			Console.WriteLine("selection has changed.");
-			if (vm != null)
-			{
-
-			}
-		}
-
-		public void addCollectionViewToPage(ScanOptionsEnum option)
-		{
-			Label label = new Label();
-			label.Text = option.GetTextForDisplay();
-			label.Margin = new Thickness(10, 20, 0, 0);
-			StackLayout.Children.Add(label);
-
-			CollectionView colView = new CollectionView();
-			colView.HeightRequest = 100;
-			colView.ItemsLayout = LinearItemsLayout.Horizontal;
-			colView.SetBinding(ItemsView.ItemsSourceProperty, vm.GetPictureListName(option));
-			StackLayout.Children.Add(colView);
-
-			colView.ItemTemplate = new DataTemplate(() =>
-			{
-				ContentView contentView = new ContentView();
-				contentView.Padding = new Thickness(2);
-
-				Image image = new Image { Aspect = Aspect.AspectFill, WidthRequest = 100 };
-				image.SetBinding(Image.SourceProperty, "Uri");
-
-				contentView.Content = image;
-				return contentView;
-			});
+			this.options = options;
 		}
 	}
 }
