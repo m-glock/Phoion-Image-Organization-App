8b1940820079bab89a88a5a8b04b3f82c2914119
m-glock <mareike.glock@t-online.de>
2020-06-30 09:05:03 +0200

    added all classes for touch recognizer

---
+++ DLuOvBamG/Services/Gestures/TouchActionEventHandler.cs
@@ -0,0 +1,4 @@
+﻿namespace DLuOvBamG.Services.Gestures
+{
+    public delegate void TouchActionEventHandler(object sender, TouchActionEventArgs args);
+}
---
+++ DLuOvBamG/Services/Gestures/TouchActionType.cs
@@ -0,0 +1,12 @@
+﻿namespace DLuOvBamG.Services.Gestures
+{
+    public enum TouchActionType
+    {
+        Entered,
+        Pressed,
+        Moved,
+        Released,
+        Exited,
+        Cancelled
+    }
+}
