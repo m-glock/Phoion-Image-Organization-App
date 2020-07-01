c102b0a0799038b537ad92f237f8671ff299bcbc
m-glock <mareike.glock@t-online.de>
2020-07-01 16:20:20 +0200

    deleted unnecessary DB Service and called other one

--- DLuOvBamG/Services/DatabaseService.cs
+++ DLuOvBamG/Services/DatabaseService.cs
@@ -4,7 +4,6 @@ using SQLiteNetExtensionsAsync.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DLuOvBamG.Services
