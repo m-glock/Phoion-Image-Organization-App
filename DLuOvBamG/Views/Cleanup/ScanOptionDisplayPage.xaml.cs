0b9e8fd8f59f6b2f48566e850a02bd2e43141425
m-glock <mareike.glock@t-online.de>
2020-07-09 19:52:41 +0200

    adapted scan option display page for different scan options

--- DLuOvBamG/Views/Cleanup/ScanOptionDisplayPage.xaml.cs
+++ DLuOvBamG/Views/Cleanup/ScanOptionDisplayPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using DLuOvBamG.Models;
+﻿using DLToolkit.Forms.Controls;
+using DLuOvBamG.Models;
 using DLuOvBamG.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -10,22 +11,77 @@ namespace DLuOvBamG.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ScanOptionDisplayPage : ContentPage
 	{
-		ScanOptionDisplayViewModel VM;
+		private ScanOptionDisplayViewModel VM;
+		private ScanOptionsEnum Option;
 
 		public ScanOptionDisplayPage(double optionValue, ScanOptionsEnum option)
 		{
 			InitializeComponent();
+			Option = option;
 			VM = BindingContext as ScanOptionDisplayViewModel;
 			List<List<Picture>> pictures = App.tf.GetAllPicturesForOption(option);
 			VM.Pictures = pictures;
-			slider.Value = optionValue;
+			Slider.Value = optionValue;
 
 			Title = option.GetTextForDisplay();
 
-            for (int i = 0; i < pictures.Count; i++)
+			AdaptViewToScanOption();
+		}
+
+		private void AdaptViewToScanOption()
+        {
+            switch (Option)
             {
-				AddCollectionViewToPage(i);
+                case ScanOptionsEnum.blurryPics:
+					// add one image grid for all blurry pictures
+					AddCollectionViewGridToPage(0);
+					break;
+                case ScanOptionsEnum.darkPics:
+					// add two image grids, one for dark and one for bright pictures
+                    for (int i = 0; i < VM.Pictures.Count; i++)
+                    {
+						AddCollectionViewGridToPage(i);
+					}
+                    break;
+                case ScanOptionsEnum.similarPics:
+					// add one collectionView for each set of similar pictures found
+					for (int i = 0; i < VM.Pictures.Count; i++)
+					{
+						AddCollectionViewToPage(i);
+					}
+					break;
             }
+        }
+
+		public void AddCollectionViewGridToPage(int groupID) {
+			
+            if (Option.Equals(ScanOptionsEnum.darkPics)){
+				Label label = new Label();
+				label.Text = groupID == 0 ? "Dunkle Bilder" : "Helle Bilder";
+				label.FontAttributes = FontAttributes.Bold;
+				label.Margin = new Thickness(10, 20, 0, 0);
+				StackLayout.Children.Add(label);
+			}
+
+			CollectionView colView = new CollectionView();
+			colView.ItemsLayout = new GridItemsLayout(3, ItemsLayoutOrientation.Vertical);
+			colView.HeightRequest = 100;
+
+			colView.ItemsSource = VM.GetPictureListForGroup(groupID);
+
+			colView.ItemTemplate = new DataTemplate(() =>
+			{
+				ContentView contentView = new ContentView();
+				contentView.Padding = new Thickness(2);
+
+				Image image = new Image { Aspect = Aspect.AspectFill, HeightRequest = 100 };
+				image.SetBinding(Image.SourceProperty, "Uri");
+
+				contentView.Content = image;
+				return contentView;
+			});
+
+			StackLayout.Children.Add(colView);
 		}
 
 		public void AddCollectionViewToPage(int groupNumber)
